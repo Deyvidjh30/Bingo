a47d413afa04e588c485e15fdc210ae0fc58b159
Deyvidjh30 <c.deyvid@escolar.ifrn.edu.br>
2025-09-16 12:59:30 +0000

    site em python

--- templates/index.py
+++ templates/index.py
@@ -5,3 +5,12 @@ st.header("Streamlit em Python")
 
 if st.button("Clique Aqui"):
     st.write("Minha primeira página me python")
+
+
+from templates.manterclienteUI import ManterClienteUI
+
+class IndexUI:
+ def main():
+  ManterClienteUI.main()
+
+IndexUI.main()
